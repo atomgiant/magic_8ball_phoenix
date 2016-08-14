b3364a0f5c7fc9ffd52db43ac51715304f218ba8
Tom Davies <atomgiant@gmail.com>
2016-08-13 23:50:04 -0400

    Move url into Magic8ball client.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,8 +2,7 @@ defmodule Magic8ballPhoenix.PageController do
   use Magic8ballPhoenix.Web, :controller
 
   def index(conn, _params) do
-    url = Magic8ballPhoenix.Router.Helpers.root_url(Magic8ballPhoenix.Endpoint, :index) <> "magic8ball/api/shake"
-    answer = Magic8ballClient.shake(url)
+    answer = Magic8ballClient.shake()
 
     conn
     |> assign(:answer, answer)
--- web/models/magic8ball_client.ex
+++ web/models/magic8ball_client.ex
@@ -1,7 +1,14 @@
 defmodule Magic8ballClient do
-  def shake(url) do
-    HTTPoison.get!(url)
+  alias Magic8ballPhoenix.Router.Helpers
+
+  def shake do
+    magic8ball_url
+    |> HTTPoison.get!
     |> handle_response
+  end
+
+  defp magic8ball_url do
+    url = Helpers.root_url(Magic8ballPhoenix.Endpoint, :index) <> "magic8ball/api/shake"
   end
 
   defp handle_response(%HTTPoison.Response{status_code: 200, body: body}) do
